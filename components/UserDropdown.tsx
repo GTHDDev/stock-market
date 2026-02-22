945295a68753b4c2c8eb1463f1f269418e0806a5
Miguel Eduardo Ovando Alvarez <miguel-workspace@outlook.com>
2026-02-21 18:27:56 -0600

    feat(auth): enhance authentication flow and integrate email notifications
    - Implement sign-in and sign-up functionalities with email verification using better-auth.
    - Add personalized welcome email feature triggered upon user registration.
    - Introduce Toaster component for user feedback on authentication actions.
    - Update layout components to manage user sessions and redirect unauthenticated users.
    - Refactor package dependencies to include new libraries for email handling and UI notifications.

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -8,20 +8,20 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 import { LogOut } from 'lucide-react'
 import NavItems from '@/components/NavItems'
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { signOut } from '@/lib/actions/auth.actions'
 
-const UserDropdown = () => {
+const UserDropdown = ({ user }: { user: User }) => {
 	const router = useRouter()
 
-	const handleSignOut = () => {
+	const handleSignOut = async () => {
+		await signOut()
 		router.push('/sign-in')
 	}
-
-	const user = { name: 'John', email: 'example@email.com' }
 
 	return (
 		<DropdownMenu>
@@ -31,12 +31,9 @@ const UserDropdown = () => {
 					className='flex items-center gap-3 text-gray-4 hover:text-yellow-500'
 				>
 					<Avatar className='h-8 w-8'>
-						<AvatarImage
-							src='https://github.com/evilrabbit.png'
-							alt='@evilrabbit'
-						/>
+						<AvatarImage src='https://avatars.githubusercontent.com/u/132961867?s=96&v=4' />
 						<AvatarFallback className='bg-yellow-500 text-yellow-900 text-sm font-bold'>
-							ER
+							{user.name[0]}
 						</AvatarFallback>
 					</Avatar>
 					<div className='hidden md:flex flex-col items-start'>
@@ -46,17 +43,13 @@ const UserDropdown = () => {
 					</div>
 				</Button>
 			</DropdownMenuTrigger>
-
 			<DropdownMenuContent className='text-gray-400'>
 				<DropdownMenuLabel>
 					<div className='flex relative items-center gap-3 py-2'>
-						<Avatar className='h-8 w-8'>
-							<AvatarImage
-								src='https://github.com/evilrabbit.png'
-								alt='@evilrabbit'
-							/>
+						<Avatar className='h-10 w-10'>
+							<AvatarImage src='https://avatars.githubusercontent.com/u/132961867?s=96&v=4' />
 							<AvatarFallback className='bg-yellow-500 text-yellow-900 text-sm font-bold'>
-								ER
+								{user.name[0]}
 							</AvatarFallback>
 						</Avatar>
 						<div className='flex flex-col'>
